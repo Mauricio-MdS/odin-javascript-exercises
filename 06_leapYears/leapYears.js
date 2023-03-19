71e17ae86db2d933521be7168c4a5aa76e0e135d
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-19 09:31:28 -0300

    feat: add exercise 6 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    return (year % 400 == 0) ? true :
+    (year % 100 == 0) ? false :
+    (year % 4 == 0) ? true :
+    false;
 
 };
 

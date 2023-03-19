6df35c1a24c3d9246546a9cfa411609c48824a63
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-19 09:19:32 -0300

    feat: add solution exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    let min = a < b ? a : b;
+    let max = a > b ? a : b;
+
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

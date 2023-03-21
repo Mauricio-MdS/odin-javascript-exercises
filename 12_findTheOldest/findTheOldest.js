d6c250fa938d9275706f7b65bd67eb23b2288b62
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-21 15:23:06 -0300

    feat: add exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a , b) => {
+        const aAge = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+        const bAge = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+        return bAge - aAge;
+    }
+    
+    );
+    console.table(sortedPeople);
+    return sortedPeople[0];
 };
 
 // Do not edit below this line

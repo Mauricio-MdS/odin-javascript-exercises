fbcc53f860a1c6edd5f10a9cd4da9dc89d764a06
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-21 14:48:51 -0300

    feat: add solution exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const processedText = text.toLowerCase().replace(/[^a-z]/g, "");
+    const reversedText = processedText.split("").reverse().join("");
+   
+    return processedText === reversedText;
 };
 
 // Do not edit below this line

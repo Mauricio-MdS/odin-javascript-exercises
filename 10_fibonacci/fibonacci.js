fe293e3f51d3bd51139c795093a0270b5be787a8
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-21 14:57:54 -0300

    feat: add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    if (x <= 2) return 1;
+    return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
